8d8d1a2f9995e547907534ccf4dd95862b5e5717
Heesoo <soyamilk0705@naver.com>
2024-05-18 17:24:13 +0900

    [create] 프로젝트 생성

---
+++ src/test/java/com/example/triptune/TriptuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triptune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
