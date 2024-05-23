194e5432f27ff10d25ad19a6b38425f57d6439ca
soyamilk0705 <soyamilk0705@naver.com>
2024-05-23 23:28:47 +0900

    [create] swagger 연동

--- src/main/java/com/triptune/home/controller/HomeController.java
+++ src/main/java/com/triptune/home/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.triptune.home.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@Tag(name = "Home", description = "메인화면 관련 API")
+@RestController
 public class HomeController {
 
     @GetMapping("/")
