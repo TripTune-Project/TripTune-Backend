35965bbf357a85f2c08626a976fea960c9405c7e
Heesoo <soyamilk0705@naver.com>
2024-06-02 19:13:38 +0900

    [docs] 이메일 인증 관련 swagger 설명 추가

--- src/main/java/com/triptune/email/controller/EmailApiController.java
+++ src/main/java/com/triptune/email/controller/EmailApiController.java
@@ -5,6 +5,7 @@ import com.triptune.common.response.ApiResponse;
 import com.triptune.email.dto.EmailDTO;
 import com.triptune.email.exception.EmailVerifyException;
 import com.triptune.email.service.EmailService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
@@ -25,6 +26,7 @@ public class EmailApiController {
     private final EmailService emailService;
 
     @PostMapping("/verify-request")
+    @Operation(summary = "이메일 인증 요청", description = "이메일 인증을 요청합니다.")
     public ApiResponse<?> verifyRequest(@Valid @RequestBody EmailDTO.VerifyRequest emailDTO) throws MessagingException {
         emailService.verifyRequest(emailDTO.getEmail());
 
@@ -32,6 +34,7 @@ public class EmailApiController {
     }
 
     @PostMapping("/verify")
+    @Operation(summary = "이메일 인증 번호 검증", description = "발급된 이메일 인증 번호를 검증합니다.")
     public ApiResponse<?> verify(@Valid @RequestBody EmailDTO.Verify emailDTO) {
         boolean isVerify = emailService.verify(emailDTO);
 
--- src/main/java/com/triptune/member/controller/MemberApiController.java
+++ src/main/java/com/triptune/member/controller/MemberApiController.java
@@ -4,6 +4,7 @@ import com.triptune.common.response.ApiResponse;
 import com.triptune.common.response.ErrorResponse;
 import com.triptune.member.dto.MemberDTO;
 import com.triptune.member.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,7 @@ public class MemberApiController {
 
 
     @PostMapping("/join")
+    @Operation(summary = "회원가입(미완)", description = "회원가입을 요청합니다.")
     public ApiResponse<MemberDTO.Response> join(@Valid @RequestBody MemberDTO.Request memberDTO){
         memberService.join(memberDTO);
 
