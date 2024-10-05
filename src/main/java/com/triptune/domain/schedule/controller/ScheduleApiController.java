328cdd52accf3468875b2a99f801ac426562fc12
Heesoo <soyamilk0705@naver.com>
2024-10-05 14:48:33 +0900

    [update] 일정 만들기 response 수정 #9

--- src/main/java/com/triptune/domain/schedule/controller/ScheduleApiController.java
+++ src/main/java/com/triptune/domain/schedule/controller/ScheduleApiController.java
@@ -1,23 +1,15 @@
 package com.triptune.domain.schedule.controller;
 
-import com.triptune.domain.member.entity.Member;
-import com.triptune.domain.schedule.dto.ScheduleRequest;
+import com.triptune.domain.schedule.dto.CreateScheduleRequest;
+import com.triptune.domain.schedule.dto.CreateScheduleResponse;
 import com.triptune.domain.schedule.service.ScheduleService;
 import com.triptune.global.response.ApiResponse;
-import com.triptune.global.service.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.nio.file.attribute.UserPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/schedules")
@@ -29,10 +21,12 @@ public class ScheduleApiController {
 
     @PostMapping
     @Operation(summary = "일정 생성", description = "여행 이름, 날짜를 선택해 일정을 생성합니다.")
-    public ApiResponse<?> createSchedule(@Valid @RequestBody ScheduleRequest scheduleRequest){
+    public ApiResponse<CreateScheduleResponse> createSchedule(@Valid @RequestBody CreateScheduleRequest createScheduleRequest){
         String userId = SecurityContextHolder.getContext().getAuthentication().getName();
 
-        scheduleService.createSchedule(scheduleRequest, userId);
-        return ApiResponse.okResponse();
+        CreateScheduleResponse response = scheduleService.createSchedule(createScheduleRequest, userId);
+        return ApiResponse.dataResponse(response);
     }
+
+
 }
