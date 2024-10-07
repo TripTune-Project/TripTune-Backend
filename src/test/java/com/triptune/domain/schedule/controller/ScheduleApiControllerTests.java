556d5e374a8d8bad8ebd6947f391dfdef50a5ce6
Heesoo <soyamilk0705@naver.com>
2024-10-07 16:49:32 +0900

    [test] 일정 조회 테스트 생성 및 공통 테스트 클래스 생성 #9

--- src/test/java/com/triptune/domain/schedule/controller/ScheduleApiControllerTests.java
+++ src/test/java/com/triptune/domain/schedule/controller/ScheduleApiControllerTests.java
@@ -1,16 +1,21 @@
 package com.triptune.domain.schedule.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.triptune.domain.BaseTest;
+import com.triptune.domain.common.entity.*;
+import com.triptune.domain.common.repository.*;
 import com.triptune.domain.member.entity.Member;
 import com.triptune.domain.member.repository.MemberRepository;
 import com.triptune.domain.schedule.dto.CreateScheduleRequest;
 import com.triptune.domain.schedule.entity.TravelAttendee;
 import com.triptune.domain.schedule.entity.TravelSchedule;
-import com.triptune.domain.schedule.enumclass.AttendeePermission;
 import com.triptune.domain.schedule.repository.AttendeeRepository;
 import com.triptune.domain.schedule.repository.ScheduleRepository;
-import com.triptune.domain.schedule.service.ScheduleService;
+import com.triptune.domain.travel.entity.TravelImage;
+import com.triptune.domain.travel.entity.TravelPlace;
+import com.triptune.domain.travel.repository.TravelImageRepository;
+import com.triptune.domain.travel.repository.TravelRepository;
+import com.triptune.global.enumclass.ErrorCode;
+import com.triptune.global.exception.DataNotFoundException;
 import com.triptune.global.util.HttpRequestEndpointChecker;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,51 +23,60 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@Transactional
-public class ScheduleApiControllerTests {
-
-    @Autowired
-    private WebApplicationContext wac;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
-    private ScheduleRepository scheduleRepository;
-
-    @MockBean
-    private AttendeeRepository attendeeRepository;
-
-    @MockBean
-    private MemberRepository memberRepository;
-
-    @MockBean
-    private HttpRequestEndpointChecker httpRequestEndpointChecker;
-
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.yml")
+public class ScheduleApiControllerTests extends BaseTest {
+    private final WebApplicationContext wac;
+    private final ScheduleRepository scheduleRepository;
+    private final AttendeeRepository attendeeRepository;
+    private final MemberRepository memberRepository;
+    private final TravelRepository travelRepository;
+    private final CountryRepository countryRepository;
+    private final CityRepository cityRepository;
+    private final DistrictRepository districtRepository;
+    private final ApiCategoryRepository apiCategoryRepository;
+    private final FileRepository fileRepository;
+    private final TravelImageRepository travelImageRepository;
     private MockMvc mockMvc;
+
+    @Autowired
+    public ScheduleApiControllerTests(WebApplicationContext wac, ScheduleRepository scheduleRepository, AttendeeRepository attendeeRepository, MemberRepository memberRepository, TravelRepository travelRepository, CountryRepository countryRepository, CityRepository cityRepository, DistrictRepository districtRepository, ApiCategoryRepository apiCategoryRepository, FileRepository fileRepository, TravelImageRepository travelImageRepository) {
+        this.wac = wac;
+        this.scheduleRepository = scheduleRepository;
+        this.attendeeRepository = attendeeRepository;
+        this.memberRepository = memberRepository;
+        this.travelRepository = travelRepository;
+        this.countryRepository = countryRepository;
+        this.cityRepository = cityRepository;
+        this.districtRepository = districtRepository;
+        this.apiCategoryRepository = apiCategoryRepository;
+        this.fileRepository = fileRepository;
+        this.travelImageRepository = travelImageRepository;
+    }
 
     @BeforeEach
     void setUp(){
@@ -71,21 +85,19 @@ public class ScheduleApiControllerTests {
                 .apply(springSecurity())
                 .alwaysDo(print())
                 .build();
+
     }
+
 
     @Test
     @DisplayName("일정 만들기 성공")
-    @WithMockUser(username = "test")
+    @WithMockUser(username = "member1")
     void createSchedule_success() throws Exception{
         // given
         CreateScheduleRequest request = createScheduleRequest();
-        TravelSchedule savedTravelSchedule = createTravelSchedule();
 
-        Member mockMember = new Member();
-        when(memberRepository.findByUserId(anyString())).thenReturn(Optional.of(mockMember));
-
-        when(scheduleRepository.save(any(TravelSchedule.class))).thenReturn(savedTravelSchedule);
-        when(attendeeRepository.save(any(TravelAttendee.class))).thenReturn(new TravelAttendee());
+        Member member1 = createMember(1L, "member1");
+        memberRepository.save(member1);
 
         // when, then
         mockMvc.perform(post("/api/schedules")
@@ -109,30 +121,87 @@ public class ScheduleApiControllerTests {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(toJsonString(request)))
                 .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.success").value(false));;
+                .andExpect(jsonPath("$.success").value(false));
     }
 
+    @Test
+    @DisplayName("일정 조회 성공 : 여행지 데이터 존재하는 경우")
+    @WithMockUser(username = "member1")
+    @DirtiesContext
+    void getScheduleWithPlaceList_success() throws Exception {
+        // given
+        TravelSchedule schedule = scheduleRepository.save(createTravelSchedule());
 
-    private CreateScheduleRequest createScheduleRequest(){
-        return CreateScheduleRequest.builder()
-                .scheduleName("테스트")
-                .startDate(LocalDate.now())
-                .endDate(LocalDate.now().plusDays(10))
-                .build();
+        Member member1 = memberRepository.save(createMember(null, "member1"));
+        Member member2 = memberRepository.save(createMember(null, "member2"));
+
+        List<TravelAttendee> attendeeList = createTravelAttendeeList(member1, member2, schedule);
+        attendeeRepository.saveAll(attendeeList);
+
+        Country country = countryRepository.save(createCountry());
+        City city = cityRepository.save(createCity(country));
+        District district = districtRepository.save(createDistrict(city, "중구"));
+        ApiCategory apiCategory = apiCategoryRepository.save(createApiCategory());
+
+        TravelPlace travelPlace = createTravelPlace(country, city, district, apiCategory);
+        travelRepository.save(travelPlace);
+
+        File file1 = fileRepository.save(createFile(null, "test1", true));
+        File file2 = fileRepository.save(createFile(null, "test2", false));;
+
+        List<TravelImage> travelImageList = createTravelImages(travelPlace, file1, file2);
+        travelImageRepository.saveAll(travelImageList);
+
+        travelPlace.setTravelImageList(travelImageList);
+        travelRepository.save(travelPlace);
+
+        // when, then
+        mockMvc.perform(get("/api/schedules/{scheduleId}", 1L)
+                .param("page", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.scheduleName").value(schedule.getScheduleName()))
+                .andExpect(jsonPath("$.data.attendeeList[0].userId").value(member1.getUserId()))
+                .andExpect(jsonPath("$.data.placeList.totalElements").value(1))
+                .andExpect(jsonPath("$.data.placeList.content[0].district").value(district.getDistrictName()))
+                .andExpect(jsonPath("$.data.placeList.content[0].placeName").value(travelPlace.getPlaceName()))
+                .andExpect(jsonPath("$.data.placeList.content[0].thumbnailUrl").value(travelImageList.get(0).getFile().getS3ObjectUrl()));
     }
 
-    private TravelSchedule createTravelSchedule(){
-        return TravelSchedule.builder()
-                .scheduleId(1L)
-                .scheduleName("테스트")
-                .startDate(LocalDate.now())
-                .endDate(LocalDate.now().plusDays(10))
-                .createdAt(LocalDateTime.now())
-                .build();
+    @Test
+    @DisplayName("일정 조회 성공 : 여행지 데이터 존재하지 않는 경우")
+    @WithMockUser(username = "member1")
+    void getScheduleWithoutPlaceList_success() throws Exception {
+        // given
+        TravelSchedule schedule = scheduleRepository.save(createTravelSchedule());
+
+        Member member1 = memberRepository.save(createMember(null, "member1"));
+        Member member2 = memberRepository.save(createMember(null, "member2"));
+
+        List<TravelAttendee> attendeeList = createTravelAttendeeList(member1, member2, schedule);
+        attendeeRepository.saveAll(attendeeList);
+
+
+        // when, then
+        mockMvc.perform(get("/api/schedules/{scheduleId}", schedule.getScheduleId())
+                        .param("page", "1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.scheduleName").value(schedule.getScheduleName()))
+                .andExpect(jsonPath("$.data.attendeeList[0].userId").exists())
+                .andExpect(jsonPath("$.data.placeList.totalElements").value(0))
+                .andExpect(jsonPath("$.data.placeList.content").isEmpty());
     }
 
-    private String toJsonString(Object obj) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(obj);
+    @Test
+    @DisplayName("일정 조회 실패 : 일정 데이터 존재하지 않는 경우")
+    @WithMockUser(username = "member1")
+    void getSchedule_notFoundException() throws Exception {
+        // given
+        // when, then
+        mockMvc.perform(get("/api/schedules/{scheduleId}", 1000L)
+                        .param("page", "1"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.message").value(ErrorCode.DATA_NOT_FOUND.getMessage()));
     }
 
 }
--- src/test/java/com/triptune/domain/schedule/service/ScheduleServiceTests.java
+++ src/test/java/com/triptune/domain/schedule/service/ScheduleServiceTests.java
@@ -1,37 +1,50 @@
 package com.triptune.domain.schedule.service;
 
+import com.triptune.domain.BaseTest;
+import com.triptune.domain.common.entity.*;
 import com.triptune.domain.member.entity.Member;
 import com.triptune.domain.member.repository.MemberRepository;
 import com.triptune.domain.schedule.dto.CreateScheduleRequest;
 import com.triptune.domain.schedule.dto.CreateScheduleResponse;
+import com.triptune.domain.schedule.dto.ScheduleResponse;
 import com.triptune.domain.schedule.entity.TravelAttendee;
 import com.triptune.domain.schedule.entity.TravelSchedule;
+import com.triptune.domain.schedule.enumclass.AttendeePermission;
+import com.triptune.domain.schedule.enumclass.AttendeeRole;
 import com.triptune.domain.schedule.repository.AttendeeRepository;
 import com.triptune.domain.schedule.repository.ScheduleRepository;
+import com.triptune.domain.travel.entity.TravelImage;
+import com.triptune.domain.travel.entity.TravelPlace;
+import com.triptune.domain.travel.repository.TravelRepository;
 import com.triptune.global.enumclass.ErrorCode;
 import com.triptune.global.exception.DataNotFoundException;
+import com.triptune.global.util.PageableUtil;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@Transactional
-@Slf4j
-public class ScheduleServiceTests {
+public class ScheduleServiceTests extends BaseTest {
 
     @InjectMocks
     private ScheduleService scheduleService;
@@ -45,20 +58,24 @@ public class ScheduleServiceTests {
     @Mock
     private AttendeeRepository attendeeRepository;
 
+    @Mock
+    private TravelRepository travelRepository;
+
 
     @Test
     @DisplayName("일정 만들기 성공")
     void createSchedule_success(){
         // given
+        String userId = "test";
         CreateScheduleRequest request = createScheduleRequest();
         TravelSchedule savedtravelSchedule = createTravelSchedule();
-        Member savedMember = createMember();
+        Member savedMember = createMember(1L, userId);
 
         when(scheduleRepository.save(any())).thenReturn(savedtravelSchedule);
         when(memberRepository.findByUserId(any())).thenReturn(Optional.of(savedMember));
 
         // when
-        CreateScheduleResponse response = scheduleService.createSchedule(request, "test");
+        CreateScheduleResponse response = scheduleService.createSchedule(request, userId);
 
         // then
         verify(attendeeRepository, times(1)).save(any(TravelAttendee.class));
@@ -71,9 +88,9 @@ public class ScheduleServiceTests {
     void createSchedule_CustomUsernameNotFoundException(){
         // given
         CreateScheduleRequest request = createScheduleRequest();
-        TravelSchedule travelSchedule = createTravelSchedule();
+        TravelSchedule schedule = createTravelSchedule();
 
-        when(scheduleRepository.save(any())).thenReturn(travelSchedule);
+        when(scheduleRepository.save(any())).thenReturn(schedule);
         when(memberRepository.findByUserId(any())).thenReturn(Optional.empty());
 
         // when
@@ -85,30 +102,100 @@ public class ScheduleServiceTests {
 
     }
 
+    @Test
+    @DisplayName("일정 조회 성공")
+    void getSchedule_success(){
+        // given
+        Country country = createCountry();
+        City city = createCity(country);
+        District district = createDistrict(city, "중구");
+        ApiCategory apiCategory = createApiCategory();
+        TravelPlace travelPlace1 = createTravelPlace(country, city, district, apiCategory);
+        TravelPlace travelPlace2 = createTravelPlace(country, city, district, apiCategory);
 
-    private CreateScheduleRequest createScheduleRequest(){
-        return CreateScheduleRequest.builder()
-                .scheduleName("테스트")
-                .startDate(LocalDate.now())
-                .endDate(LocalDate.now().plusDays(10))
-                .build();
+        File file1 = createFile(1L, "test1", true);
+        File file2 = createFile(2L, "test2", false);
+
+        List<TravelImage> travelImageList1 = createTravelImages(travelPlace1, file1, file2);
+        travelPlace1.setTravelImageList(travelImageList1);
+        List<TravelImage> travelImageList2 = createTravelImages(travelPlace2, file1, file2);
+        travelPlace2.setTravelImageList(travelImageList2);
+
+        List<TravelPlace> placeList = createTravelPlaceList(travelPlace1, travelPlace2);
+
+        TravelSchedule schedule = createTravelSchedule();
+        Member member1 = createMember(1L, "member1");
+        Member member2 = createMember(2L, "member2");
+
+
+        List<TravelAttendee> attendeeList = createTravelAttendeeList(member1, member2, schedule);
+
+        Pageable pageable = PageableUtil.createPageRequest(1, 5);
+
+        when(scheduleRepository.findByScheduleId(any())).thenReturn(Optional.of(schedule));
+        when(travelRepository.findAllByAreaData(any(), anyString(), anyString(), anyString()))
+                .thenReturn(new PageImpl<>(placeList, pageable, 1));
+        when(attendeeRepository.findAllByTravelSchedule_ScheduleId(any())).thenReturn(attendeeList);
+
+        // when
+        ScheduleResponse response = scheduleService.getSchedule(schedule.getScheduleId(), 1);
+
+        // then
+        assertEquals(response.getScheduleName(), schedule.getScheduleName());
+        assertEquals(response.getCreatedAt(), schedule.getCreatedAt());
+        assertEquals(response.getAttendeeList().get(0).getUserId(), attendeeList.get(0).getMember().getUserId());
+        assertEquals(response.getAttendeeList().get(0).getRole(), attendeeList.get(0).getRole().name());
+        assertEquals(response.getPlaceList().getTotalElements(), placeList.size());
+        assertEquals(response.getPlaceList().getContent().get(0).getPlaceName(), placeList.get(0).getPlaceName());
+        assertNotNull(response.getPlaceList().getContent().get(0).getThumbnailUrl());
     }
 
-    private TravelSchedule createTravelSchedule(){
-        return TravelSchedule.builder()
-                .scheduleId(1L)
-                .scheduleName("테스트")
-                .startDate(LocalDate.now())
-                .endDate(LocalDate.now().plusDays(10))
-                .createdAt(LocalDateTime.now())
-                .build();
+    @Test
+    @DisplayName("일정 조회 성공: 여행지 데이터 없는 경우")
+    void getScheduleWithoutPlaceList_success(){
+        // given
+        Country country = createCountry();
+        City city = createCity(country);
+        District district = createDistrict(city, "중구");
+        ApiCategory apiCategory = createApiCategory();
+
+        TravelSchedule schedule = createTravelSchedule();
+        Member member1 = createMember(1L, "member1");
+        Member member2 = createMember(2L, "member2");
+
+        List<TravelAttendee> attendeeList = createTravelAttendeeList(member1, member2, schedule);
+
+        Pageable pageable = PageableUtil.createPageRequest(1, 5);
+
+        when(scheduleRepository.findByScheduleId(any())).thenReturn(Optional.of(schedule));
+        when(travelRepository.findAllByAreaData(any(), anyString(), anyString(), anyString()))
+                .thenReturn(new PageImpl<>(new ArrayList<>(), pageable, 0));
+        when(attendeeRepository.findAllByTravelSchedule_ScheduleId(any())).thenReturn(attendeeList);
+
+        // when
+        ScheduleResponse response = scheduleService.getSchedule(schedule.getScheduleId(), 1);
+
+        // then
+        assertEquals(response.getScheduleName(), schedule.getScheduleName());
+        assertEquals(response.getCreatedAt(), schedule.getCreatedAt());
+        assertEquals(response.getAttendeeList().get(0).getUserId(), attendeeList.get(0).getMember().getUserId());
+        assertEquals(response.getAttendeeList().get(0).getRole(), attendeeList.get(0).getRole().name());
+        assertEquals(response.getPlaceList().getTotalElements(), 0);
+        assertTrue(response.getPlaceList().getContent().isEmpty());
     }
 
-    private Member createMember(){
-        return Member.builder()
-                .memberId(1L)
-                .userId("test")
-                .build();
+    @Test
+    @DisplayName("일정 조회 실패: 일정을 찾을 수 없는 경우")
+    void getSchedule_notFoundException(){
+        // given
+        when(scheduleRepository.findByScheduleId(any())).thenReturn(Optional.empty());
+
+        // when
+        DataNotFoundException fail = assertThrows(DataNotFoundException.class, () -> scheduleService.getSchedule(0L, 1));
+
+        // then
+        assertEquals(fail.getHttpStatus(), ErrorCode.DATA_NOT_FOUND.getStatus());
+        assertEquals(fail.getMessage(), ErrorCode.DATA_NOT_FOUND.getMessage());
     }
 
 }
