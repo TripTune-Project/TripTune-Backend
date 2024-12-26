4235095659b07036c6192e042d4115c441e689db
Heesoo <soyamilk0705@naver.com>
2024-12-26 14:43:24 +0900

    [update] cors 설정 수정 및 테스트 오류 수정

--- src/main/java/com/triptune/global/config/WebConfig.java
+++ src/main/java/com/triptune/global/config/WebConfig.java
@@ -1,20 +1,29 @@
 package com.triptune.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+import java.util.List;
 
-    @Value("${cors.allowed-origins}")
-    private String[] allowedOrigins;
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class WebConfig implements WebMvcConfigurer {
+    private List<String> allowedOrigins;
+
+    public List<String> getAllowedOrigins(){
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(List<String> allowedOrigins){
+        this.allowedOrigins = allowedOrigins;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins)
+                .allowedOrigins(allowedOrigins.toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
                 .allowedHeaders("Authorization", "Content-Type")
                 .exposedHeaders("Custom-Header")    // 클라이언트 측 응답에서 노출되는 해더 지정
