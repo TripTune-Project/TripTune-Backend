2ef612124c0cf88a19a2812c50087a91e935dfed
Heesoo <soyamilk0705@naver.com>
2025-01-26 00:38:46 +0900

    [refactor] 기본 프로필 이미지 s3 이미지로 변경 #3

--- src/test/java/com/triptune/domain/member/service/ProfileImageServiceTest.java
+++ src/test/java/com/triptune/domain/member/service/ProfileImageServiceTest.java
@@ -1,23 +1,15 @@
 package com.triptune.domain.member.service;
 
-import com.triptune.domain.common.service.S3Service;
 import com.triptune.domain.member.entity.ProfileImage;
 import com.triptune.domain.member.repository.ProfileImageRepository;
-import com.triptune.global.enumclass.ErrorCode;
-import com.triptune.global.exception.DataNotFoundException;
+import com.triptune.global.properties.DefaultProfileImageProperties;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.io.File;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -28,11 +20,11 @@ class ProfileImageServiceTest {
     @Autowired
     private ProfileImageService profileImageService;
 
-    @MockBean
-    private S3Service s3Service;
-
     @Autowired
     private ProfileImageRepository profileImageRepository;
+
+    @Autowired
+    private DefaultProfileImageProperties profileImageProperties;
 
 
     @Test
@@ -45,17 +37,17 @@ class ProfileImageServiceTest {
         assertThat(response.getOriginalName()).isEqualTo(DEFAULT_PROFILE_IMAGE_NAME);
         assertThat(response.getFileSize()).isEqualTo(DEFAULT_PROFILE_IMAGE_SIZE);
     }
-
-    @Test
-    @DisplayName("기본 프로필 이미지 파일 가져오기")
-    void getDefaultProfileImageFile(){
-        // given, when
-        File response = profileImageService.getDefaultProfileImageFile();
-
-        // then
-        assertThat(response.length()).isEqualTo(DEFAULT_PROFILE_IMAGE_SIZE);
-        assertThat(response.getName()).isEqualTo(DEFAULT_PROFILE_IMAGE_NAME);
-    }
+//
+//    @Test
+//    @DisplayName("기본 프로필 이미지 파일 가져오기")
+//    void getDefaultProfileImageFile(){
+//        // given, when
+//        File response = profileImageService.getDefaultProfileImageFile();
+//
+//        // then
+//        assertThat(response.length()).isEqualTo(DEFAULT_PROFILE_IMAGE_SIZE);
+//        assertThat(response.getName()).isEqualTo(DEFAULT_PROFILE_IMAGE_NAME);
+//    }
 
 
 }
