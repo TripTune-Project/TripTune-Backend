187a63a6c10667e535a69b97b852ca1f75d72e93
Heesoo <soyamilk0705@naver.com>
2025-01-31 15:31:54 +0900

    [update] 일정 저장 시 날짜 유효성 검사 제외 #12

--- src/main/java/com/triptune/domain/schedule/dto/request/ScheduleUpdateRequest.java
+++ src/main/java/com/triptune/domain/schedule/dto/request/ScheduleUpdateRequest.java
@@ -1,6 +1,5 @@
 package com.triptune.domain.schedule.dto.request;
 
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -24,7 +23,6 @@ public class ScheduleUpdateRequest {
     private LocalDate startDate;
 
     @NotNull(message = "여행지 종료 날짜는 필수 입력 값입니다.")
-    @FutureOrPresent(message = "오늘 이후 날짜만 입력 가능합니다.")
     private LocalDate endDate;
 
     private List<RouteRequest> travelRoute = new ArrayList<>();
