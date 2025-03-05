3ec616e37436fa5df6c49d11e2c6c45c1f8399a7
Heesoo <soyamilk0705@naver.com>
2025-03-05 18:33:24 +0900

    [refactor] jpa 매핑 정보 수정

--- src/main/java/com/triptune/schedule/enumclass/AttendeeRole.java
+++ src/main/java/com/triptune/schedule/enumclass/AttendeeRole.java
@@ -1,16 +1,8 @@
 package com.triptune.schedule.enumclass;
 
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 public enum AttendeeRole {
-
-    AUTHOR(1, "작성자"),
-    GUEST(2, "게스트");
-
-
-    private final int id;
-    private final  String role;
+    AUTHOR, GUEST;
 
     public boolean isAuthor(){
         return this == AUTHOR;
