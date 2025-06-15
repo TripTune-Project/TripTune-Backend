5d7d8c18202471b974c093b48217be3979b2e5ce
Heesoo <soyamilk0705@naver.com>
2025-06-15 16:34:41 +0900

    [fix] validation 추가 #149

--- src/main/java/com/triptune/schedule/repository/ChatMessageRepository.java
+++ src/main/java/com/triptune/schedule/repository/ChatMessageRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
     Page<ChatMessage> findAllByScheduleId(Pageable pageable, @Param("scheduleId") Long scheduleId);
     List<ChatMessage> findAllByScheduleId(@Param("scheduleId") Long scheduleId);
