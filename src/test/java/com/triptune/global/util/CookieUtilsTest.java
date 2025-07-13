7cb414c93d3beab5fce717bdccb4bb04c3f257ab
Heesoo <soyamilk0705@naver.com>
2025-07-13 18:58:19 +0900

    [test] 쿠키 httpOnly 테스트

--- src/test/java/com/triptune/global/util/CookieUtilsTest.java
+++ src/test/java/com/triptune/global/util/CookieUtilsTest.java
@@ -47,22 +47,22 @@ class CookieUtilsTest {
         assertThat(response).contains("SameSite=None");
     }
 
-//
-//    @Test
-//    @DisplayName("refreshToken 쿠키 생성")
-//    void createRefreshTokenCookie() {
-//        // given
-//        // when
-//        String response = CookieUtils.createCookie(CookieType.REFRESH_TOKEN, "refreshTokenValue");
-//
-//        // then
-//        assertThat(response).contains(CookieType.REFRESH_TOKEN.getKey() + "=refreshTokenValue");
-//        assertThat(response).contains("Max-Age=" + CookieType.REFRESH_TOKEN.getMaxAgeSeconds());
-//        assertThat(response).contains("Path=/");
-//        assertThat(response).contains("HttpOnly");
-//        assertThat(response).contains("Secure");
-//        assertThat(response).contains("SameSite=None");
-//    }
+
+    @Test
+    @DisplayName("refreshToken 쿠키 생성")
+    void createRefreshTokenCookie() {
+        // given
+        // when
+        String response = cookieUtils.createCookie(CookieType.REFRESH_TOKEN, "refreshTokenValue");
+
+        // then
+        assertThat(response).contains(CookieType.REFRESH_TOKEN.getKey() + "=refreshTokenValue");
+        assertThat(response).contains("Max-Age=" + CookieType.REFRESH_TOKEN.getMaxAgeSeconds());
+        assertThat(response).contains("Path=/");
+        assertThat(response).contains("HttpOnly");
+        assertThat(response).contains("Secure");
+        assertThat(response).contains("SameSite=None");
+    }
 
 
     @Test
