569f81b1dff5902bcaeb5b2276224614ae7e690a
Heesoo <soyamilk0705@naver.com>
2026-07-10 00:38:33 +0900

    [update] 지역명 변경 #261

--- src/main/java/com/triptune/travel/enums/CityType.java
+++ src/main/java/com/triptune/travel/enums/CityType.java
@@ -11,14 +11,14 @@ import java.util.Set;
 @Getter
 @AllArgsConstructor
 public enum CityType {
-    ALL("all", Set.of("서울", "부산", "제주도", "경기도", "강원특별자치도", "경상북도", "경상남도", "전북특별자치도", "전라남도", "충청북도", "충청남도")),
-    SEOUL( "seoul", Set.of("서울")),
-    BUSAN( "busan", Set.of("부산")),
-    JEJU( "jeju", Set.of("제주도")),
+    ALL("all", Set.of("서울특별시", "부산광역시", "제주특별자치도", "경기도", "강원특별자치도", "경상북도", "경상남도", "전북특별자치도", "전남광주통합특별시", "충청북도", "충청남도")),
+    SEOUL( "seoul", Set.of("서울특별시")),
+    BUSAN( "busan", Set.of("부산광역시")),
+    JEJU( "jeju", Set.of("제주특별자치도")),
     GYEONGGI("gyeonggi", Set.of("경기도")),
     GANGWON("gangwon", Set.of("강원특별자치도")),
     GYEONGSANG("gyeongsang", Set.of("경상북도", "경상남도")),
-    JEOLLA("jeolla", Set.of("전북특별자치도", "전라남도")),
+    JEOLLA("jeolla", Set.of("전북특별자치도", "전남광주통합특별시")),
     CHUNGCHEONG("chungcheong", Set.of("충청북도", "충청남도"));
 
     private final String value;
